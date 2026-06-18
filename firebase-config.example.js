ef935342cf8cbc36ee2601ee9c6f40e23303b5d3
Fame <fameplanb@gmail.com>
2026-06-18 15:43:50 -0400

    Add files via upload

---
+++ firebase-config.example.js
@@ -0,0 +1,12 @@
+// Beacon Wars Firebase config placeholder.
+// Copy this file to firebase-config.js after creating a Firebase web app.
+// Do NOT commit real keys publicly.
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Beacon Wars Firebase config.
+// Safe to load on the client. Firestore Rules + Anonymous Auth protect the database.
+window.firebaseConfig = {
+  apiKey: "AIzaSyAwKzgziXDLe8oQ4kCPehPFmnda3OUNgrQ",
+  authDomain: "beacon-wars.firebaseapp.com",
+  projectId: "beacon-wars",
+  storageBucket: "beacon-wars.firebasestorage.app",
+  messagingSenderId: "747008612345",
+  appId: "1:747008612345:web:17d80b559cbe936ff38f28",
+  measurementId: "G-0CCWCH08W9"
+};
